04917462fd9a4989aab30ae9bb56ef3d6d64a5bd
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2024-02-14 21:12:19 +0900

    Section6 Access

---
+++ src/access/b/PublicClassOuterMain.java
@@ -0,0 +1,12 @@
+package access.b;
+
+import access.a.PublicClass;
+
+public class PublicClassOuterMain {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+
+//        DefaultCalss1 defaultCalss1 = new DefaultCalss1();
+        // 패키지 pack.a 다른 패키지다.
+    }
+}
